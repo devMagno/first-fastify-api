332eded6b9bf5f401d34e957856aa7ab75e66b7d
Guilherme Magno <guilhermertfmagno@gmail.com>
2023-03-11 16:12:34 -0300

    feat: database using knex and SQLite

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("⚠️ Environment variables are not valid", _env.error.format());
+
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { knex } from "./database";
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', (req, res) => {
-  return 'Hello World!'
-})
+app.get("/hello", async (req, res) => {
+  const transaction = await knex("transactions")
+    .where("amount", 1000)
+    .select("*");
+
+  return transaction;
+});
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP Server Running!')
-})
+  console.log("HTTP Server Running!");
+});
